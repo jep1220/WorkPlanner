d8f898911e758b2b32365516e5618652a45dc2c5
Ernie Perez <ernieperez96@gmail.com>
2021-01-13 23:31:26 -0800

    add save button

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
-  // listen for save button clicks
+
+
+  // jumbotron current date
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+
+  // save button click
   $(".saveBtn").on("click", function() {
-    // get nearby values
+    
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // save in localStorage
+    // save action
     localStorage.setItem(time, value);
   });
+
+   // get saved data
+   $("#9AM .description").val(localStorage.getItem("9AM"));
+   $("#10AM .description").val(localStorage.getItem("10AM"));
+   $("#11AM .description").val(localStorage.getItem("11AM"));
+   $("#12PM .description").val(localStorage.getItem("12PM"));
+   $("#1PM .description").val(localStorage.getItem("1PM"));
+   $("#2PM .description").val(localStorage.getItem("2PM"));
+   $("#3PM .description").val(localStorage.getItem("3PM"));
+   $("#4PM .description").val(localStorage.getItem("4PM"));
+   $("#5PM .description").val(localStorage.getItem("5PM"));
+ 
 
   function hourUpdater() {
     // get current number of hours
@@ -17,7 +36,6 @@ $(document).ready(function() {
     $(".time-block").each(function() {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      // check if we've moved past this time
       if (blockHour < currentHour) {
         $(this).addClass("past");
       } 
@@ -34,21 +52,8 @@ $(document).ready(function() {
   }
 
   hourUpdater();
+  var interval = setInterval(hourUpdater, 2000);
 
-  // set up interval to check if current time needs to be updated
-  var interval = setInterval(hourUpdater, 15000);
-
-  // load any saved data from localStorage
-  $("#9AM .description").val(localStorage.getItem("9AM"));
-  $("#10AM .description").val(localStorage.getItem("10AM"));
-  $("#11AM .description").val(localStorage.getItem("11AM"));
-  $("#12PM .description").val(localStorage.getItem("12PM"));
-  $("#1PM .description").val(localStorage.getItem("1PM"));
-  $("#2PM .description").val(localStorage.getItem("2PM"));
-  $("#3PM .description").val(localStorage.getItem("3PM"));
-  $("#4PM .description").val(localStorage.getItem("4PM"));
-  $("#5PM .description").val(localStorage.getItem("5PM"));
-
-  // display current day on page
-  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+ 
+  
 });
